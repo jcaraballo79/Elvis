b5309c67c3178aeedab502b36f76dc35ff69c5db
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-06-04 17:57:35 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Jorge Caraballo on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
